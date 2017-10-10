29756729ff9541e4126cfbbcad0488a9cf87f033
Mark Allen <mrallen1@yahoo.com>
2017-10-09 23:39:09 -0500

    Shell for Week 5 files

--- Week 5/files.py
+++ Week 5/files.py
@@ -3,17 +3,10 @@
 def count_lines(filename):
     """given a filename, this function should return a count of the number of
     lines in the file"""
-    lines = 0
-    with open(filename, "rb") as f:
-        for line in f:
-            lines += 1
-
-    return lines
+    pass
 
 def write_file(filename):
     """given a filename, this function should write at least two lines into the file"""
-    with open(filename, "w") as f:
-        f.write("Hello!\n")
-        f.write("Line 2\n")
+    pass
 
 
